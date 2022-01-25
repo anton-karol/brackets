8515587eba16a198f82f2e9b5fcc78a1315d54de
Anton <anton.karol@gmail.com>
2022-01-25 20:50:36 +0300

    init: first draft

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let arr = str.split('');
+    for (let brackets in bracketsConfig) {
+        if (arr.filter(x => x === brackets[0]).length !== arr.filter(x => x === brackets[1]).length) {
+            return false;
+        }
+    }
+    return true;
 }
