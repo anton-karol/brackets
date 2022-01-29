d355e6ae2f9875e4127a8b490ab62f09e6e10668
Anton <anton.karol@gmail.com>
2022-01-29 17:48:38 +0300

    fix: final

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 module.exports = function check(str, bracketsConfig) {
-    let arr = str.split('');
-    for (let brackets in bracketsConfig) {
-        if (arr.filter(x => x === brackets[0]).length !== arr.filter(x => x === brackets[1]).length) {
-            return false;
+    let prev = '';
+    while (prev.length !== str.length) {
+        prev = str;
+        for (let brackets of bracketsConfig) {
+            str = str.replace(brackets[0] + brackets[1], '');
         }
     }
-    return true;
+    return (str.length === 0);
 }
